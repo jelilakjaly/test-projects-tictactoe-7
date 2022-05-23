939a7335ce18b7e05781027558882c5829d5ff5c
Jelil Akjaly <jelil.akjaly@gmail.com>
2022-05-24 01:29:51 +0430

    Finished the game

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-
 data Cell = E | X | O deriving (Eq)
 
 instance Show Cell where 
@@ -44,6 +43,10 @@ instance Show Board where
 
 data Player = PlayerO | PlayerX deriving (Eq)
 
+instance Show Player where 
+    show PlayerO = "Player O"
+    show PlayerX = "Player x"
+
 getWinner :: Board -> Maybe Player
 getWinner = undefined
 
@@ -68,7 +71,100 @@ playerWon (Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player =
           PlayerO -> O
           PlayerX -> X
 
+boardIsFull :: Board -> Bool 
+boardIsFull (Board c1 c2 c3 c4 c5 c6 c7 c8 c9) =
+    c1 /= E && c2 /= E && c3 /= E &&
+    c4 /= E && c5 /= E && c6 /= E &&
+    c7 /= E && c8 /= E && c9 /= E
+
+-- clears the terminal on linux
+clearScreen :: IO ()
+clearScreen = putStr "\ESC[2J"
+
+
+strToNumber :: String -> Maybe Int 
+strToNumber n = case n of 
+    "1" -> Just 1
+    "2" -> Just 2
+    "3" -> Just 3
+    "4" -> Just 4
+    "5" -> Just 5
+    "6" -> Just 6
+    "7" -> Just 7
+    "8" -> Just 8
+    "9" -> Just 9
+    _   -> Nothing
+
+updateBoard :: Board -> Player -> Int -> Board 
+updateBoard board@(Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player num = 
+    case num of
+        1 -> if c1 == E
+                then Board (cellOfPlayer player) c2 c3 c4 c5 c6 c7 c8 c9
+                else board
+        2 -> if c2 == E
+                then Board c1 (cellOfPlayer player) c3 c4 c5 c6 c7 c8 c9
+                else board
+        3 -> if c3 == E
+                then Board c1 c2 (cellOfPlayer player) c4 c5 c6 c7 c8 c9
+                else board
+        4 -> if c4 == E
+                then Board c1 c2 c3 (cellOfPlayer player) c5 c6 c7 c8 c9
+                else board
+        5 -> if c5 == E
+                then Board c1 c2 c3 c4 (cellOfPlayer player) c6 c7 c8 c9
+                else board
+        6 -> if c6 == E
+                then Board c1 c2 c3 c4 c5 (cellOfPlayer player) c7 c8 c9
+                else board
+        7 -> if c7 == E
+                then Board c1 c2 c3 c4 c5 c6 (cellOfPlayer player) c8 c9
+                else board
+        8 -> if c8 == E
+                then Board c1 c2 c3 c4 c5 c6 c7 (cellOfPlayer player) c9
+                else board
+        9 -> if c9 == E
+                then Board c1 c2 c3 c4 c5 c6 c7 c8 (cellOfPlayer player)
+                else board
+        _ -> board
+
+    where 
+        cellOfPlayer :: Player -> Cell
+        cellOfPlayer player = case player of 
+            PlayerO -> O
+            playerX -> X
+
+nextPlayer :: Player -> Player 
+nextPlayer player = case player of
+  PlayerO -> PlayerX
+  PlayerX -> PlayerO
+
+updateGame :: Board -> Player -> IO ()
+updateGame board player = do 
+    clearScreen
+    putStrLn (show board)
+    if playerOWon board 
+        then do 
+            putStrLn "Player O wins."
+            return ()
+        else if playerXWon board 
+                 then do 
+                     putStrLn "Player X wins."
+                     return ()
+                 else if boardIsFull board 
+                          then do
+                              putStrLn "Nobody wins!"
+                              return ()
+                           else do 
+                              putStrLn (show player ++ ", mark your position (1-9)")
+                              numStr <- getLine 
+                              case strToNumber numStr of
+                                  Nothing -> updateGame board player
+                                  Just n -> updateGame (updateBoard board player n) (nextPlayer player)
+
 
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    let board = Board E E E E E E E E E
+    let player = PlayerO
+    updateGame board player
