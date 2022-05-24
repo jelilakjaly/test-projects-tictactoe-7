073a45f6645a26b83a1a7bcd4ca22775e64d1e5e
Jelil Akjaly <jelil.akjaly@gmail.com>
2022-05-25 00:34:03 +0430

    Moved more code from Main to Board

--- app/Board.hs
+++ app/Board.hs
@@ -42,3 +42,43 @@ instance Show Board where
 
 
 data Player = PlayerO | PlayerX 
+
+instance Show Player where 
+    show PlayerO = "Player O"
+    show PlayerX = "Player x"
+
+
+getWinner :: Board -> Maybe Player
+getWinner = undefined
+
+playerOWon :: Board -> Bool
+playerOWon b = playerWon b PlayerO
+
+playerXWon :: Board -> Bool
+playerXWon b = playerWon b PlayerX
+
+playerWon :: Board -> Player -> Bool 
+playerWon (Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player = 
+    (c1 == c && c2 == c && c3 == c) ||
+    (c4 == c && c5 == c && c6 == c) ||
+    (c7 == c && c8 == c && c9 == c) ||
+    (c1 == c && c4 == c && c7 == c) ||
+    (c2 == c && c5 == c && c8 == c) ||
+    (c3 == c && c6 == c && c9 == c) ||
+    (c1 == c && c5 == c && c9 == c) ||
+    (c3 == c && c5 == c && c7 == c)
+    where
+        c = case player of
+          PlayerO -> O
+          PlayerX -> X
+
+boardIsFull :: Board -> Bool 
+boardIsFull (Board c1 c2 c3 c4 c5 c6 c7 c8 c9) =
+    c1 /= E && c2 /= E && c3 /= E &&
+    c4 /= E && c5 /= E && c6 /= E &&
+    c7 /= E && c8 /= E && c9 /= E
+
+nextPlayer :: Player -> Player 
+nextPlayer player = case player of
+  PlayerO -> PlayerX
+  PlayerX -> PlayerO
--- app/Main.hs
+++ app/Main.hs
@@ -4,39 +4,9 @@ module Main where
 import Board
 
 
-instance Show Player where 
-    show PlayerO = "Player O"
-    show PlayerX = "Player x"
 
-getWinner :: Board -> Maybe Player
-getWinner = undefined
 
-playerOWon :: Board -> Bool
-playerOWon b = playerWon b PlayerO
 
-playerXWon :: Board -> Bool
-playerXWon b = playerWon b PlayerX
-
-playerWon :: Board -> Player -> Bool 
-playerWon (Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player = 
-    (c1 == c && c2 == c && c3 == c) ||
-    (c4 == c && c5 == c && c6 == c) ||
-    (c7 == c && c8 == c && c9 == c) ||
-    (c1 == c && c4 == c && c7 == c) ||
-    (c2 == c && c5 == c && c8 == c) ||
-    (c3 == c && c6 == c && c9 == c) ||
-    (c1 == c && c5 == c && c9 == c) ||
-    (c3 == c && c5 == c && c7 == c)
-    where
-        c = case player of
-          PlayerO -> O
-          PlayerX -> X
-
-boardIsFull :: Board -> Bool 
-boardIsFull (Board c1 c2 c3 c4 c5 c6 c7 c8 c9) =
-    c1 /= E && c2 /= E && c3 /= E &&
-    c4 /= E && c5 /= E && c6 /= E &&
-    c7 /= E && c8 /= E && c9 /= E
 
 -- clears the terminal on linux
 clearScreen :: IO ()
@@ -94,10 +64,7 @@ updateBoard board@(Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player num =
             PlayerO -> O
             playerX -> X
 
-nextPlayer :: Player -> Player 
-nextPlayer player = case player of
-  PlayerO -> PlayerX
-  PlayerX -> PlayerO
+
 
 updateGame :: Board -> Player -> IO ()
 updateGame board player = do 
