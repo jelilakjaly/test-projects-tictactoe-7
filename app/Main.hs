136496a3ca9af2f760e347ed17e73215a65a15a4
Jelil Akjaly <jelil.akjaly@gmail.com>
2022-05-28 01:59:33 +0430

    Updated updateBoard function

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 
-import Board
+import           Board
 
 
 
@@ -13,8 +13,8 @@ clearScreen :: IO ()
 clearScreen = putStr "\ESC[2J"
 
 
-strToNumber :: String -> Maybe Int 
-strToNumber n = case n of 
+strToNumber :: String -> Maybe Int
+strToNumber n = case n of
     "1" -> Just 1
     "2" -> Just 2
     "3" -> Just 3
@@ -26,67 +26,50 @@ strToNumber n = case n of
     "9" -> Just 9
     _   -> Nothing
 
-updateBoard :: Board -> Player -> Int -> Board 
-updateBoard board@(Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player num = 
-    case num of
-        1 -> if c1 == E
-                then Board (cellOfPlayer player) c2 c3 c4 c5 c6 c7 c8 c9
-                else board
-        2 -> if c2 == E
-                then Board c1 (cellOfPlayer player) c3 c4 c5 c6 c7 c8 c9
-                else board
-        3 -> if c3 == E
-                then Board c1 c2 (cellOfPlayer player) c4 c5 c6 c7 c8 c9
-                else board
-        4 -> if c4 == E
-                then Board c1 c2 c3 (cellOfPlayer player) c5 c6 c7 c8 c9
-                else board
-        5 -> if c5 == E
-                then Board c1 c2 c3 c4 (cellOfPlayer player) c6 c7 c8 c9
-                else board
-        6 -> if c6 == E
-                then Board c1 c2 c3 c4 c5 (cellOfPlayer player) c7 c8 c9
-                else board
-        7 -> if c7 == E
-                then Board c1 c2 c3 c4 c5 c6 (cellOfPlayer player) c8 c9
-                else board
-        8 -> if c8 == E
-                then Board c1 c2 c3 c4 c5 c6 c7 (cellOfPlayer player) c9
-                else board
-        9 -> if c9 == E
-                then Board c1 c2 c3 c4 c5 c6 c7 c8 (cellOfPlayer player)
-                else board
-        _ -> board
+-- update the cell only if it is empty
+updateBoard :: Board -> Player -> Int -> Board
+updateBoard board@(Board c1 c2 c3 c4 c5 c6 c7 c8 c9) player num
+    | num == 1 && c1 == E = Board (cellOfPlayer player) c2 c3 c4 c5 c6 c7 c8 c9
+    | num == 2 && c2 == E = Board c1 (cellOfPlayer player) c3 c4 c5 c6 c7 c8 c9
+    | num == 3 && c3 == E = Board c1 c2 (cellOfPlayer player) c4 c5 c6 c7 c8 c9
+    | num == 4 && c4 == E = Board c1 c2 c3 (cellOfPlayer player) c5 c6 c7 c8 c9
+    | num == 5 && c5 == E = Board c1 c2 c3 c4 (cellOfPlayer player) c6 c7 c8 c9
+    | num == 6 && c6 == E = Board c1 c2 c3 c4 c5 (cellOfPlayer player) c7 c8 c9
+    | num == 7 && c7 == E = Board c1 c2 c3 c4 c5 c6 (cellOfPlayer player) c8 c9
+    | num == 8 && c8 == E = Board c1 c2 c3 c4 c5 c6 c7 (cellOfPlayer player) c9
+    | num == 9 && c9 == E = Board c1 c2 c3 c4 c5 c6 c7 c8 (cellOfPlayer player)
+    | otherwise           = board
 
 
 
 updateGame :: Board -> Player -> IO ()
-updateGame board player = do 
+updateGame board player = do
     clearScreen
     putStrLn (show board)
-    if playerOWon board 
-        then do 
+    if playerOWon board
+        then do
             putStrLn "Player O wins."
             return ()
-        else if playerXWon board 
-                 then do 
-                     putStrLn "Player X wins."
-                     return ()
-                 else if boardIsFull board 
-                          then do
-                              putStrLn "Nobody wins!"
-                              return ()
-                           else do 
-                              putStrLn (show player ++ ", mark your position (1-9)")
-                              numStr <- getLine 
-                              case strToNumber numStr of
-                                  Nothing -> updateGame board player
-                                  Just n -> updateGame (updateBoard board player n) (nextPlayer player)
+        else if playerXWon board
+            then do
+                putStrLn "Player X wins."
+                return ()
+            else if boardIsFull board
+                then do
+                    putStrLn "Nobody wins!"
+                    return ()
+                else do
+                    putStrLn (show player ++ ", mark your position (1-9)")
+                    numStr <- getLine
+                    case strToNumber numStr of
+                        Nothing -> updateGame board player
+                        Just n  -> updateGame (updateBoard board player n)
+                                              (nextPlayer player)
 
 
 
 main :: IO ()
-main = do 
-    let board = Board E E E E E E E E E
+main = do
+    let board  = Board E E E E E E E E E
     let player = PlayerO
     updateGame board player
